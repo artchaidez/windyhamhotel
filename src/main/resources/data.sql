c4c723e59b79a29c3b2f9150993edb6e1a53285b
artchaidez <artchaidez23@yahoo.com>
2021-04-24 11:33:56 -0500

    Trying stuff
    Broke something. Branching it and reverting back in main branch

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+--Fake data to test the tables
+
+--INSERT INTO GUEST VALUES (1, 'Arturo Chaidez', 'artchaidez23@yahoo.com', '(708)770-3210');
+--INSERT INTO Reservation VALUES (1, 2, 0, '2020-04-01', '2020-04-08', 'Room1', 'Arturo Chaidez');
+--INSERT INTO Employee VALUES (10, 'password');
+--INSERT INTO Room VALUES (1, 100, 'Room1', 5000.00, 'Nice room', 'Open');
