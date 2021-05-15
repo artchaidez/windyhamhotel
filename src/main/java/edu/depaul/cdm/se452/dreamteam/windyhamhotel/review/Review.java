bba207260f512759ae233af254d30c46296b06a4
ASadRam <adams@Adams-MBP.local>
2021-05-15 14:30:54 -0500

    This one got left behind earlier...

---
+++ src/main/java/edu/depaul/cdm/se452/dreamteam/windyhamhotel/review/Review.java
@@ -0,0 +1,68 @@
+package edu.depaul.cdm.se452.dreamteam.windyhamhotel.review;
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int reviewID;
+
+    private String name;
+    private String rating;
+    private String date;
+    private String comment;
+
+
+    public Review(){}
+
+    public Review(String name, String rating, String date, String comment) {
+        this.name = name;
+        this.rating = rating;
+        this.date = date;
+        this.comment = comment;
+    }
+
+    public int getReviewID() {
+        return reviewID;
+    }
+
+    public void setReviewID(int reviewID) {
+        this.reviewID = reviewID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
