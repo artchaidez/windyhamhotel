fb78d58af5f6b4ac8d1e110674ad78e36ce1a072
hongli <hxue44@gmail.com>
2021-05-21 01:15:23 -0500

    update

--- src/main/java/edu/depaul/cdm/se452/dreamteam/windyhamhotel/complaint/Complaint.java
+++ src/main/java/edu/depaul/cdm/se452/dreamteam/windyhamhotel/complaint/Complaint.java
@@ -27,4 +27,27 @@ public class Complaint {
     }
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getProblem() {
+        return problem;
+    }
+
+    public void setProblem(String problem) {
+        this.problem = problem;
+    }
 }
